6b31eb86863fc3a87b1ad4802e863037b3a6caf9
fmaiolo <buzzrocket@yahoo.com>
2017-06-22 18:14:11 -0400

    Added the euler function with url variable input

--- euler-1.php
+++ euler-1.php
@@ -1,1 +1,17 @@
-
+<?php
+function euler($limit) {
+	$sum = 0;
+	for($x = 0; $x < $limit; $x++) {
+		if($x % 3 === 0 || $x % 5 === 0) {
+			$sum += $x;
+		}
+	}
+	return $sum;
+}
+$limit = 1000;
+$input = $_GET["limit"];
+if(!empty($input)) {
+	$limit = $input;
+}
+echo euler($limit);
+?>
