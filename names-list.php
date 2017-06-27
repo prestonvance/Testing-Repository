d5394fb72e444eefe947ca76b451871f578001af
prestonvance <pvance@eyerisemarketing.com>
2017-06-27 17:03:52 -0400

    Update names-list.php
    added parameters for text filters

--- names-list.php
+++ names-list.php
@@ -1,10 +1,18 @@
 <?php
 	$names = array("Sig", "Nikki", "Jack", "Nina", "Désìre", "Matchbox 20", "Matchbox 100");
+
 	if(isset($_GET["sort"])){
 		$sort=$_GET["sort"];
 	}else{
 		$sort=1;
 	}
+
+	if(isset($_GET["keyword"])){
+		$Keyword=$_GET["keyword"];
+	}else{
+		$Keyword='';
+	}
+
 	$x=1;
 	if($sort==1){
 		sort($names, SORT_NATURAL | SORT_FLAG_CASE);
@@ -12,7 +20,12 @@
 		rsort($names, SORT_NATURAL | SORT_FLAG_CASE);
 	}
 	foreach($names as $Key=>$Value){
-		echo "$x: $Value<br>";
-		$x++;
+		if($Keyword!='' AND $Value==$Keyword){
+			echo "$x: $Value<br>";
+			$x++;
+		}else{
+			echo "$x: $Value<br>";
+			$x++;
+		}
 	}
 ?>
