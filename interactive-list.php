3b6993e68afd095829a5d7bdfa577e2d86502041
prestonvance <pvance@eyerisemarketing.com>
2017-06-27 11:45:57 -0400

    Update interactive-list.php

--- interactive-list.php
+++ interactive-list.php
@@ -9,8 +9,12 @@ The list should be able to:
 
 <?php
 	$names = array("Sig", "Nikki", "Jack", "Nina", "Désìre", "Matchbox 20", "Matchbox 100");
-	sort($names);
+	$sort=1;
 	$x=1;
+
+	if($sort==1){
+		sort($names);
+	}
 	foreach($names as $Key=>$Value){
 		echo "$x: $Value<br>";
 		$x++;
