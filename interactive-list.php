ba99e4d915ca654a955d2f9a57f15756e8794972
fmaiolo <buzzrocket@yahoo.com>
2017-06-27 11:16:38 -0400

    Adding a new interactive list project for @prestonvance

---
+++ interactive-list.php
@@ -0,0 +1,12 @@
+Below is an interactive list of names.
+<br/>
+The list should be able to:
+<ul>
+	<li>Sort (with proper number sort)</li>
+	<li>Show index (starting with 1)</li>
+	<li>... (to be continued)</li>
+</ul>
+TODO: Place list here
+<?php
+	$names = array("Sig", "Nikki", "Jack", "Nina", "Désìre", "Matchbox 20", "Matchbox 100");
+?>
