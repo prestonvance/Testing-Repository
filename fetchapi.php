3ac8644a07b02783c600cc778e5eec37b854663b
fmaiolo <buzzrocket@yahoo.com>
2017-06-27 16:14:34 -0400

    Refining fetch logic

--- fetchapi.php
+++ fetchapi.php
@@ -1,9 +1,33 @@
+Below is an interactive list of names.
+<br/>
+The list should be able to:
+<ul>
+	<li>Sort (with proper number sort)</li>
+	<li>Show index (starting with 1)</li>
+	<li>... (to be continued)</li>
+</ul>
+<form>
+	<select id='sortfilter' name='sort' ONCHANGE='fetchList()'>
+		<option value=''>No Sort</option>
+		<option value='1' selected>Sort Low to High</option>
+		<option value='2'>Sort High to Low</option>
+	</select>
+</form>
+<div id="listContainer"></div>
 <script>
-fetch("zphp/ajax/sortednames.php") 
-.then(function() {
-    echo console.log(response);
-})
-.catch(function() {
-    // This is where you run code if the server returns any errors
-});
+function fetchList() {
+	var sort = document.getElementById("sortfilter").value;
+	fetch("interactive-list.php?sort="+ sort).then(function(response) {
+		// response.text() returns a promise that we chain on to read the text off the stream
+	    response.text().then(function(text) {
+	    	console.log(text);
+	    	document.getElementById("listContainer").innerHTML = text;
+		});
+	}).catch(function(error) {
+	    console.log(error);
+	});
+};
+
+// initialize the list
+fetchList();
 </script>
--- interactive-list.php
+++ interactive-list.php
@@ -1,27 +1,7 @@
-Below is an interactive list of names.
-<br/>
-The list should be able to:
-<ul>
-	<li>Sort (with proper number sort)</li>
-	<li>Show index (starting with 1)</li>
-	<li>... (to be continued)</li>
-</ul>
-<form>
-	<select id='sortfilter' name='sort' ONCHANGE='this.form.submit()'>
-		<option value=''>No Sort</option>
-		<option value='1' selected>Sort Low to High</option>
-		<option value='2'>Sort High to Low</option>
-	</select>
-</form>
 <?php
 	$names = array("Sig", "Nikki", "Jack", "Nina", "Désìre", "Matchbox 20", "Matchbox 100");
 	if(isset($_GET["sort"])){
 		$sort=$_GET["sort"];
-		?>
-		<script>
-		document.getElementById("sortfilter").value='<?php echo $_GET["sort"] ?>';
-		</script>
-		<?php
 	}else{
 		$sort=1;
 	}
@@ -36,5 +16,3 @@ The list should be able to:
 		$x++;
 	}
 ?>
-
-Output can be seen at http://hysteriamachinesentertainment.com/php-test
