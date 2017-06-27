0a3479e15f25d574f0017d381598e3142a09e3bb
prestonvance <pvance@eyerisemarketing.com>
2017-06-27 15:45:47 -0400

    Update fetchapi.php
    deleted echo

--- fetchapi.php
+++ fetchapi.php
@@ -1,7 +1,7 @@
 <script>
 fetch("zphp/ajax/sortednames.php") 
 .then(function() {
-    echo console.log(response);
+    console.log(response);
 })
 .catch(function() {
     // This is where you run code if the server returns any errors
