84e668226cd0d04b4bd623dd506b6a8a214cc6b4
prestonvance <pvance@eyerisemarketing.com>
2017-06-27 12:20:34 -0400

    Update interactive-list.php

--- interactive-list.php
+++ interactive-list.php
@@ -9,25 +9,32 @@ The list should be able to:
 <form>
 	<select id='sortfilter' name='sort' ONCHANGE='this.form.submit()'>
 		<option value=''>No Sort</option>
-		<option value='1'>Sort Low to High</option>
+		<option value='1' selected>Sort Low to High</option>
 		<option value='2'>Sort High to Low</option>
 	</select>
 </form>
 <?php
 	$names = array("Sig", "Nikki", "Jack", "Nina", "Désìre", "Matchbox 20", "Matchbox 100");
-	$sort=1;
-	$sort=$_GET["sort"];
+	if(isset($_GET["sort"])){
+		$sort=$_GET["sort"];
+		?>
+		<script>
+		document.getElementById("sortfilter").value='<?php echo $_GET["sort"] ?>';
+		</script>
+		<?php
+	}else{
+		$sort=1;
+	}
 	$x=1;
 	if($sort==1){
 		sort($names, SORT_NATURAL | SORT_FLAG_CASE);
 	}elseif($sort==2){
-		rsort($names, SORT_NATURAL | Sort_FLAG_CASE);
+		rsort($names, SORT_NATURAL | SORT_FLAG_CASE);
 	}
 	foreach($names as $Key=>$Value){
 		echo "$x: $Value<br>";
 		$x++;
 	}
 ?>
-<script>
-document.getElementById("sortfilter").value='<?php echo $_GET["sort"] ?>';
-</script>
+
+Output can be seen at http://http://hysteriamachinesentertainment.com/php-test
